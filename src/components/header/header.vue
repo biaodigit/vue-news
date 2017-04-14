e8bae8c67da8fdef8039a90da6b4455f6c3674c1
wendigit <15622285015@163.com>
2017-04-14 16:11:40 +0800

    完成公共头部组件开发

---
+++ src/components/header/header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <span class="navigation" @click="showSidebar"></span><h2>{{title}}</h2>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props:{
+        title:{
+            type:String,
+            default:'今日热闻'
+        }
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods:{
+        showSidebar() {
+            this.$emit('showSide');
+        }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+   @import '../../common/stylus/mixin.styl'
+
+  .header
+    position fixed
+    top 0
+    left 0
+    font-size 0
+    width 100%
+    height 40px
+    z-index 1
+    background-color rgb(2,143,214)
+    & > h2
+      line-height 40px
+      font-size 22px
+      color white
+    .navigation
+      position absolute
+      top 10px
+      width 20px
+      height 20px
+      left 10px
+      padding 5px
+      bg-image('navigation')
+      background-size 20px 20px
+      background-repeat no-repeat
+
+</style>
---
+++ src/components/homepage/homepage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="homepage">
+    <v-header @showSide="show" :title="title"></v-header>
+    <sidebar :sidebarShow="sidebarShow"></sidebar>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from '../header/header.vue'
+  import sidebar from '../sidebar/sidebar.vue'
+
+  export default {
+      data() {
+        return {
+            title:'今日热闻',
+            sidebarShow:false
+        }
+      },
+      methods:{
+        show() {
+           this.sidebarShow = !this.sidebarShow;
+        }
+      },
+      components:{
+          'v-header':header,
+          sidebar
+      }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
