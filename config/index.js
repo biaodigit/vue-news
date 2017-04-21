2185c7def8282af8f3f97e4934dbcb1f5fa36369
wendigit <15622285015@163.com>
2017-04-22 00:18:12 +0800

    上传忘记修改的跨域配置

--- config/index.js
+++ config/index.js
@@ -27,7 +27,15 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api':{
+        target:'http://news-at.zhihu.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api/4'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
