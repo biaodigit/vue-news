43c32881ad65238daab92006a31021c67c959e2b
wendigit <15622285015@163.com>
2017-04-25 18:01:42 +0800

    对各组件和vuex文件进行注释

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,42 +4,51 @@
 import * as types from './mutation_types'
 
 export default {
+  //改变当前主题id
   changeCurrentThemeId:({ commit },id) => {
     commit(types.CHANGE_CURRENT_THEME_ID,id)
   },
+  //增加首页新闻数组和首页新闻id数组
   addNews:({ commit },payload) => {
     commit(types.ADD_NEWS,payload)
   },
+  //改变当前新闻详情页id
   addNewId:({commit},id) => {
     commit(types.ADD_NEWID,id)
   },
+  //改变第一次进入首页状态
   changeFirstLoad:({ commit }) => {
     commit(types.CHANGE_FIRST_LOAD)
   },
-  changeFirstSide:({ commit }) => {
-    commit(types.CHANGE_FIRST_SIDEBAR)
-  },
+  //下拉刷新数据后删除原有数组的新闻
   deleteData:({commit}) => {
     commit(types.DELETE_STORIES);
   },
+  //添加详情页底部状态到数组
   changeStoryExtra:({commit},extra) => {
     commit(types.STORY_EXTRA,extra)
   },
+  //判断收藏状态
   judgeCollectState:({commit}) => {
     commit(types.JUDGE_COLLECT_STATE);
   },
+  //改变收藏状态
   changeCollectState:({commit}) => {
     commit(types.CHANGE_COLLECT_STATE)
   },
+  //改变跳转详情页的路由状态，判断是从哪里跳入
   changeGoType:({commit},id) => {
     commit(types.CHANGE_GO_TYPES,id)
   },
+  //增加主题新闻数据
   addTheme:({commit},payload) => {
     commit(types.ADD_THEME,payload)
   },
+  //改变主编id
   addEditorId:({commit},payload) => {
     commit(types.ADD_EDITOR_ID,payload)
   },
+  //增加全部加载过的新闻到数组
   addAllNews:({commit},stories) => {
     commit(types.ADD_ALL_NEWS,stories)
   }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,31 +4,35 @@
 import * as types from './mutation_types'
 
 export default {
+  //改变当前主题id
   [types.CHANGE_CURRENT_THEME_ID](state,id){
      state.currentThemeId = id;
   },
+  //增加首页新闻数组和首页新闻id数组
   [types.ADD_NEWS](state,payload){
     state.stories = state.stories.concat(payload.stories);
     state.ids = state.ids.concat(payload.ids);
   },
+  //改变当前新闻详情页id
   [types.ADD_NEWID](state,id){
     state.id = id;
   },
+  //改变第一次进入首页状态
   [types.CHANGE_FIRST_LOAD](state){
     state.isFirstLoad = !state.isFirstLoad;
   },
-  [types.CHANGE_FIRST_SIDEBAR](state){
-    state.isFirstSide = !state.isFirstSide
-  },
+  //下拉刷新数据后删除原有数组的新闻
   [types.DELETE_STORIES](state){
     state.stories = [];
   },
+  //添加详情页底部状态到数组
   [types.STORY_EXTRA](state,extra){
     state.comments = extra.comments;
     state.long_comments = extra.long_comments;
     state.short_comments = extra.short_comments;
     state.popularity = extra.popularity
   },
+  //判断收藏状态
   [types.JUDGE_COLLECT_STATE](state){
     if(state.isCollectIds.indexOf(state.id) < 0){
        state.isCollect = false;
@@ -36,6 +40,7 @@ export default {
        state.isCollect = true;
     }
   },
+  //改变收藏状态
   [types.CHANGE_COLLECT_STATE](state){
     let index = state.isCollectIds.indexOf(state.id)
     if(index < 0){
@@ -52,15 +57,19 @@ export default {
       state.isCollectNews.splice(index,1)
     }
   },
+  //改变跳转详情页的路由状态，判断是从哪里跳入
   [types.CHANGE_GO_TYPES](state,id){
     state.goType = id;
   },
+  //增加主题新闻数据
   [types.ADD_THEME](state,payload){
     state.currentTheme = payload;
   },
+  //改变主编id
   [types.ADD_EDITOR_ID](state,payload){
     state.editor = payload;
   },
+  //增加全部加载过的新闻到数组
   [types.ADD_ALL_NEWS](state,stories){
     state.allStories = state.allStories.concat(stories)
   }
