2e1294d3698741a95c3243752017e9f446d4b2c6
迪 <15622285015@163.com>
2017-06-05 23:55:24 +0800

    修改端口

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 80,
+    port: 8000,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
