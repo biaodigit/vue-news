fdeece325b926ee28c888961b22dd256713f609e
luowendi <15622285015@163.com>
2017-12-17 11:46:20 +0800

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/components/HomePage/HomePage'
-import ThemeDetail from '@/components/ThemeDetail/ThemeDetail'
-import NewDetail from '@/components/NewDetail/NewDetail'
-import Collect from '@/components/Collect/Collect'
-import Comments from '@/components/Comments/Comments'
-import EditorsList from '@/components/EditorsList/EditorsList'
-import Editor from '@/components/Editor/Editor'
+import HomePage from 'components/HomePage/HomePage'
+import ThemeDetail from 'components/ThemeDetail/ThemeDetail'
+import NewDetail from 'components/NewDetail/NewDetail'
+import Collect from 'components/Collect/Collect'
+import Comments from 'components/Comments/Comments'
+import EditorsList from 'components/EditorsList/EditorsList'
+import Editor from 'components/Editor/Editor'
 
 Vue.use(Router)
 
